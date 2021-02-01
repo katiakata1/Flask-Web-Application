e84b8d6536a757c73068f59f39e581d4ef83368c
Jekaterina <bujanovakatia@gmail.com>
2021-02-01 17:48:30 +0000

    configuring git branches

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ class User(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.id
 
+class Supplies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 
@@ -37,10 +45,59 @@ def login():
 @app.route('/welcome', methods=['GET', 'Post'])
 def welcome():
     return render_template('welcome.html')
- 
+
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Supplies(content = task_content)
+
+        try: 
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else: 
+        tasks = Supplies.query.order_by(Supplies.date_created).all()
+        return render_template('welcome.html', tasks = tasks)
+
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Supplies.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deliting your task'
+
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Supplies.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except: 
+            return 'There was an issue updating yout task'
+    else: 
+        return render_template('update.html', task=task)
 
 if __name__=='__main__':
-    app.run(debug=True, host = '0.0.0.0')
+    app.run(debug=True)
 
 
 
